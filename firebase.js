928f9c2c78ec1a9b3e5d31f6255656dbb3b36c4e
Piyushkushwah2003 <Piyushkushwah2003@gmail.com>
2025-06-22 22:37:23 +0530

    Feat: added some change

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
 // firebase.js
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
-
+console.log("🔐 FIREBASE_PROJECT_ID exists:", !!process.env.FIREBASE_PROJECT_ID);
+console.log("🔐 FIREBASE_PRIVATE_KEY exists:", !!process.env.FIREBASE_PRIVATE_KEY);
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
